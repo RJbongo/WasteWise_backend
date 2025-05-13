da829b49a64117e53458122395c7504a720a6d1f
Reige <reigejambong@gmail.com>
2025-05-13 22:28:12 +0800

    device

--- bin_status/admin.py
+++ bin_status/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import BinStatus, Device
 
-# Register your models here.
-admin.site.register(BinStatus)
-admin.site.register(Device)
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'api_key']     
+    readonly_fields = ['api_key']
+
+@admin.register(BinStatus)
+class BinStatusAdmin(admin.ModelAdmin):
+    list_display = ['device', 'bin_type', 'is_full', 'updated_at']
