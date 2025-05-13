3c40cfd21024baede29415e5a93969ae7be140a2
Reige <reigejambong@gmail.com>
2025-05-13 22:21:36 +0800

    last

--- bin_status/admin.py
+++ bin_status/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BinStatus
+from .models import BinStatus, Device
 
 # Register your models here.
 admin.site.register(BinStatus)
+admin.site.register(Device)
