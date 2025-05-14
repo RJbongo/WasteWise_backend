f706fb9b98ecaeb6a36773d645c9c2fa673135dc
Reige <reigejambong@gmail.com>
2025-05-14 17:24:06 +0800

    boang man diay ka

--- bin_status/urls.py
+++ bin_status/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import BinStatusViewSet, latest_bin_status 
+from .views import BinStatusViewSet
 from django.urls import path, include
 
 router = DefaultRouter()
@@ -7,5 +7,4 @@ router.register(r'bin-status', BinStatusViewSet, basename='bin-status')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('bin-status/latest/', latest_bin_status), 
 ]
