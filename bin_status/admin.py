e18c6a7b806e8c334b697e7872b85ae17de5e36b
Reige <reigejambong@gmail.com>
2025-05-14 12:50:34 +0800

    user model

--- bin_status/admin.py
+++ bin_status/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import BinStatus
+from django.contrib.auth.models import User
 
 @admin.register(BinStatus)
 class BinStatusAdmin(admin.ModelAdmin):
     list_display = ['bio_status', 'recyclable_status', 'updated_at']
+
+
+admin.site.register(User)
