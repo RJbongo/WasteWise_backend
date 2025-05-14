76a11237f2d7fe777fd364b7ba5579978c153d06
Reige <reigejambong@gmail.com>
2025-05-14 14:27:36 +0800

    maybe last

--- bin_status/views.py
+++ bin_status/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from .models import BinStatus
 from .serializers import BinStatusSerializer
+
 
 class BinStatusViewSet(viewsets.ModelViewSet):
     queryset = BinStatus.objects.all()
@@ -9,5 +12,15 @@ class BinStatusViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action in ['create']:
-            return [AllowAny()]  # Allow unauthenticated POST from IoT
+            return [AllowAny()] 
         return [IsAuthenticated()]
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def latest_bin_status(request):
+    latest_status = BinStatus.objects.last()
+    if latest_status:
+        serializer = BinStatusSerializer(latest_status)
+        return Response(serializer.data)
+    return Response({"detail": "No bin status available."}, status=404)
